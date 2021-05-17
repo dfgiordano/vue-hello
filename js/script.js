7671390be770b2bbf14f39811550fa750292a075
dfgiordano <federico.giordano90@gmail.com>
2021-05-17 15:14:41 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* Stampare a schermo un messaggio all'interno di un h1, utilizzando i data */
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "", 
+            image:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5WFARW9WyGVzxRfzx_VCNrjaiCbsgo0C_4_hBeNWSgkqyTS4jCo8oq4dkULBISBhkXeM&usqp=CAU",
+            
+    },
+});
